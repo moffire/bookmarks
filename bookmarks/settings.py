b1cec5f4d69c9a80fe9a9020f104bd373a914cc7
moffire <alexeydudarev.spb@gmail.com>
2020-08-24 17:57:50 +0300

    created user login/logout

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -116,6 +116,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+# Auth
+
+LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
